02a3fbed852e67d6eefbdd98b890e424128e50ec
ntduonggg <92130368+ntduonggg@users.noreply.github.com>
2025-08-06 21:50:41 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,40 @@
+<p align="center">
+  <a href="https://www.uit.edu.vn/" title="Trường Đại học Công nghệ Thông tin" style="border: 5;">
+    <img src="https://i.imgur.com/WmMnSRt.png" alt="Trường Đại học Công nghệ Thông tin | University of Information Technology">
+  </a>
+</p>
+
+<!-- Title -->
+<h1 align="center"><b>IS403.M21.HTCL Phân tích dữ liệu kinh doanh</b></h1>
+
+
+
+## BẢNG MỤC LỤC
+* [ Giới thiệu môn học](#gioithieumonhoc)
+* [Quá trình môn học](#quatrinh)
+* [ Giảng viên hướng dẫn](#giangvien)
+* [ Thành viên nhóm](#thanhvien)
+* [ Đồ án môn học](#doan)
+## GIỚI THIỆU MÔN HỌC
+<a name="gioithieumonhoc"></a>
+* **Tên môn học**: Phân tích dữ liệu kinh doanh
+* **Mã môn học**: IS403
+* **Lớp học**: IS403.M21.HTCL
+* **Năm học**: 2021-2022
+## GIẢNG VIÊN HƯỚNG DẪN
+<a name="giangvien"></a>
+* **Nguyễn Đình Thuân**
+* **Nguyễn Minh Nhựt**
+
+## THÀNH VIÊN NHÓM
+<a name="thanhvien"></a>
+| STT    | MSSV          | Họ và Tên              |Email                   |
+| ------ |:-------------:| ----------------------:|-------------------------:
+| 1      | 19520636      | Lưu Trần Anh Khoa      |19520636@gm.uit.edu.vn   |
+| 2      | 19521410      | Nguyễn Thái Dương      |19521410@gm.uit.edu.vn   |
+| 3      | 19521065      | Trần Thanh Trung       |19521065@gm.uit.edu.vn   |
+## ĐỒ ÁN MÔN HỌC
+<a name="doan"></a>
+1. Tên đồ án: Cryptocurrency Price Predictions
+
+
---
+++ main.py
@@ -0,0 +1,58 @@
+from algorithms import *
+from flask import Flask, render_template, jsonify
+from werkzeug.exceptions import HTTPException
+
+app = Flask(__name__)
+
+def get_candles(df, df_pred):
+    data = []
+    data_pred = []
+    try:
+        for row in df_pred.iterrows():
+            row_data = {'date': row[1]['Datetime'], 'close': row[1]['Close']}
+            data_pred.append(row_data)
+        for row in df.iterrows():
+            row_data = {'date': row[1]['Datetime'], 'close': row[1]['Close']}
+            data.append(row_data)
+    except Exception as e:
+        print(e)
+    return [data, data_pred]
+
+
+@app.route('/getJson/<ticker>/<type>', methods=['GET'])
+def get_json_data(ticker='BTC-USD',type = 'LSTM' ):
+    df, df_pred = crypto_data(ticker, '3mo', "60m") 
+    df = df.dropna()
+    df.reset_index(inplace=True)
+    df['Datetime'] = df['Datetime'].dt.strftime('%Y-%m-%d %H:%M:%S')
+
+    if type == 'LSTM':
+        pred_LSTM, rmse_lstm = lstm(df)
+    else:
+        pred_LSTM, rmse_lstm = arima(df)
+
+    array = df[['Close']].to_numpy(dtype='float')
+    final = np.append(array, pred_LSTM)
+    df_pred['Close'] = final.tolist()
+    df_pred.reset_index(inplace=True)
+    df_pred['Datetime'] = df_pred['Datetime'].dt.strftime('%Y-%m-%d %H:%M:%S')
+
+    response = jsonify(get_candles(df, df_pred.tail(15))) 
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('plot.html')
+
+@app.errorhandler(Exception)
+def internal_error(error):
+    code = 500
+    print(error.args)
+    if isinstance(error, HTTPException):
+        code = error.code
+    return '', code
+
+
+app.run(host="0.0.0.0", port=5000, debug=True)
